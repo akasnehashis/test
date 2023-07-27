b3176b6cdbfa7e92e30977546108fa04bf28bd20
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2023-07-27 11:46:42 +0530

    Add files via upload

---
+++ scripts/appstart.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ #npm start /home/ubuntu/node/server/server.js
+
+ cd /home/ubuntu/interview_admin-main
+
+ #pm2 restart interview_admin 
---
+++ scripts/installpkgs.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /home/ubuntu/interview_admin-main
+
+# git pull && sudo npm install
